5e90251363548f7826e0d59b1fe5ee9fca5e23b1
sarai romero <sarairomero1025@hotmail.com>
2019-11-12 19:05:37 -0500

    clase registro agregada, error en nom de la clase

---
+++ imports/ui/components/registro.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    style="background-image: url('https://images.unsplash.com/photo-1458682625221-3a45f8a844c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'); widht:100%; height: 100%;  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;"
+  >
+    <v-container>
+      <v-row class="pb-4">
+        <v-col>
+          <menuBar />
+        </v-col>
+      </v-row>
+
+      <v-card max-width="800" class="mx-auto">
+        <v-card-title>
+          <span class="headline ml-6 mt-5">Registrarse</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row class="ml-6 mr-6">
+              <v-col md="6">
+                <v-text-field label="Nombre*" required></v-text-field>
+              </v-col>
+              <v-col md="6">
+                <v-text-field label="Apellido*" required></v-text-field>
+              </v-col>
+              <v-col md="12">
+                <v-text-field label="Correo*" required></v-text-field>
+              </v-col>
+              <v-col md="6">
+                <v-text-field label="Número Identificacion*" v-mask="'################'" required></v-text-field>
+              </v-col>
+               <v-col md="6">
+                <v-text-field label="Celular" v-mask="'###-###-####'" required></v-text-field>
+              </v-col>
+              <v-col md="6">
+                <v-text-field
+                  :append-icon="show3 ? 'visibility' : 'visibility_off'"
+                  :type="show3 ? 'text' : 'password'"
+                  label="Contraseña*"
+                  hint="Mínimo 8 caracteres"
+                  value
+                  class="input-group--focused"
+                  required
+                  @click:append="show3 = !show3"
+                ></v-text-field>
+              </v-col>
+              <v-col md="6">
+                <v-text-field
+                  :append-icon="show4 ? 'visibility' : 'visibility_off'"
+                  :type="show4 ? 'text' : 'password'"
+                  name="input-10-2"
+                  label="Confirmar contraseña*"
+                  hint="Mínimo 8 caracteres"
+                  value
+                  class="input-group--focused"
+                  required
+                  @click:append="show4 = !show4"
+                ></v-text-field>
+              </v-col>
+              <v-col md="12">
+                <v-checkbox label="Soy despachador"></v-checkbox>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small class="ml-5">*Campos obligatorios</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="mr-5 mb-5" color="blue darken-1" text>Cancelar</v-btn>
+          <v-btn class="mr-5 mb-5" color="blue darken-1" text>Guardar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Menu from "../components/menu";
+import { mask } from "vue-the-mask";
+
+export default {
+  components: {
+    menuBar: Menu
+  },
+  directives: {
+    mask
+  },
+  data: function() {
+    return {
+      show3: false,
+      show4: false
+    };
+  }
+};
+</script>
+
+<style>
+</style>
